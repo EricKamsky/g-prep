64d4e1438ff5d7720917ecb7637f3a5037cece42
EricKamsky <erickamsky@gmail.com>
2018-11-16 10:36:27 -0800

    dupe test

--- rotated-array-search.py
+++ rotated-array-search.py
@@ -30,7 +30,15 @@ def test_search_rotated_sorted_not_exists():
     assert index == -1
 
 
+def test_search_sorted_dupe_exists():
+    nums = [1, 2, 3, 4, 5, 5]
+    index = search(nums, target=5)
+    # first sequential occurrence
+    assert index == 4
+
+
 test_search_sorted_exists()
 test_search_rotated_sorted_exists()
 test_search_sorted_not_exists()
 test_search_rotated_sorted_not_exists()
+test_search_sorted_dupe_exists()
